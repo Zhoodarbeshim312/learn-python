1e703236bdd9461b970187ca00fb960e4c8c565d
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-11-21 18:23:02 +0600

    practic dict

--- practic.py
+++ practic.py
@@ -6,3 +6,10 @@
 
 # total = newCars1.intersection(newCars2)
 # print(len(total))
+
+cars = {"tesla": 5000, "BMW": 45000, "Audi": 1000}
+carName = input("Унаанын атын жаз: ")
+if carName in cars:
+    print(f"баасы", cars.values())
+else:
+    print("Мындай машина жок")
